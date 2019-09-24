e362397a9e8c840dca13b19472d7b84aab3ff753
xera agana-woodbine <xeraagana-woodbine@xeras-MacBook-Pro.local>
2019-09-24 15:51:59 -0400

    added xcode project

---
+++ Swiftdemo/Swiftdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftdemo
+//
+//  Created by xera agana-woodbine on 9/24/19.
+//  Copyright © 2019 xera agana-woodbine. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
